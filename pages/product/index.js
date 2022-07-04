96fb5b58e4fa024402e3645a1b9b5b787ee65058
shabaaz27 <khanshabaaz05@gmail.com>
2022-07-04 12:23:12 +0530

    added a tag

--- pages/product/index.js
+++ pages/product/index.js
@@ -6,14 +6,14 @@ function ProductList(){
       <a>Home</a>
     </Link>
        
-    <Link  href="/product/1">
-    <h2>Product 1</h2>
+    <Link href="/product/1">
+    <a>Product 1</a>
     </Link>
-    <Link  href="/product/2" replace>
-    <h2>Product 2</h2>
+    <Link href="/product/2" replace>
+    <a>Product 2</a>
     </Link>
-    <Link  href="/product/3">
-    <h2>Product 3</h2>
+    <Link href="/product/3">
+    <a>Product 3</a>
     </Link>
     </>
 }
