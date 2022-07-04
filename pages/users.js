c8df2214e7a1880faa8875f0afe357c14cb6ab3a
shabaaz27 <khanshabaaz05@gmail.com>
2022-07-04 15:19:48 +0530

    getStaticProps

---
+++ pages/users.js
@@ -0,0 +1,37 @@
+function UserList({users}){
+   return <>
+     <h1>List Of users</h1>
+    {
+        users?.map(user=>{
+            return (
+                <div key={user.id}>
+                   <p>{user.name}</p> 
+                   <p>{user.email}</p> 
+                </div>
+            )
+        })
+    }
+    </>
+}
+
+
+export default UserList
+
+export async function getStaticProps() {
+    // Call an external API endpoint to get posts.
+    // You can use any data fetching library
+    const response = await fetch('https://jsonplaceholder.typicode.com/users')
+    const data = await response.json()
+  
+    // By returning { props: { posts } }, the Blog component
+    // will receive `posts` as a prop at build time
+    return {
+      props: {
+       users : data
+      }
+    }
+  }
+  
+
+
+
