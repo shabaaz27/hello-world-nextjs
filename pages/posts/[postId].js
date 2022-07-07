ae7e5a0f97b726232d9f538fda349a8c235097ec
shabaaz27 <khanshabaaz05@gmail.com>
2022-07-07 12:35:18 +0530

    fetch getStaticPaths for Dynamic pages

--- pages/posts/[postId].js
+++ pages/posts/[postId].js
@@ -16,18 +16,30 @@ export default Post
 
 
 export async function getStaticPaths(){
+    const response = await fetch(`https://jsonplaceholder.typicode.com/posts`)
+
+    const data = await response.json()
+    const paths = data.map(post =>{
+        return{
+            params:{
+                postId:`${post.id}`        
+            }
+        }
+    })
+
     return{
-        paths:[
-            {
-            params:{postId:'1'}
-            },
-            {
-                params:{postId:'2'}
-                },
-                {
-                    params:{postId:'3'}
-                    }
-        ],
+        // paths:[
+        //     {
+        //     params:{postId:'1'}
+        //     },
+        //     {
+        //         params:{postId:'2'}
+        //         },
+        //         {
+        //             params:{postId:'3'}
+        //             }
+        // ],
+        paths,
         fallback:false,
     }
 }
