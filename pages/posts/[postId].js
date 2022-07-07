2bb817b99643260d752dd0321568b1c1227b6e93
shabaaz27 <khanshabaaz05@gmail.com>
2022-07-07 17:07:49 +0530

    fallback true in getStaticPaths

--- pages/posts/[postId].js
+++ pages/posts/[postId].js
@@ -1,8 +1,13 @@
 import Link from 'next/link'
-
+import { useRouter } from 'next/router'
 
 
 function Post({post}) {
+    const router = useRouter();
+
+    if(router.isFallback) {
+        return <h1>Loading....</h1>
+    }
   return(
         <>
             <h1>List of Posts</h1>
@@ -26,29 +31,36 @@ export async function getStaticPaths(){
             }
         }
     })
-
+console.log(paths)
     return{
-        // paths:[
-        //     {
-        //     params:{postId:'1'}
-        //     },
-        //     {
-        //         params:{postId:'2'}
-        //         },
-        //         {
-        //             params:{postId:'3'}
-        //             }
-        // ],
-        paths,
-        fallback:false,
+        paths:[
+            {
+            params:{postId:'1'}
+            },
+            {
+                params:{postId:'2'}
+                },
+                {
+                    params:{postId:'3'}
+                    }
+        ],
+        // paths,
+        fallback:true,
     }
 }
 
 export const getStaticProps = async (context) =>{
     const {params} = context
-    const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.postId}`)
+    const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${params?.postId}`)
 
     const data = await response.json()
+    if(!data.id){
+        return {
+            notFound: true,
+        }
+
+    }
+    console.log(`/posts/${params.postId}`)
     return{
         props:{
             post:data
